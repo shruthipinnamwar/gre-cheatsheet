93594c64c78b0f75f5bb0720e67ee4f9cf65023e
Anil Cherukuri <anil.cherukuri03@gmail.com>
2017-11-29 13:01:33 -0600

    makes external API call for loading the user details

--- app/src/main/java/com/example/anil/myapplication/MainActivity.java
+++ app/src/main/java/com/example/anil/myapplication/MainActivity.java
@@ -1,23 +1,20 @@
 package com.example.anil.myapplication;
-
-
-
-
-
-import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
-import android.view.Menu;
+import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 
+
 public class MainActivity extends Activity {
     Button button;
+    String[] mobileArray = {"Word1","Word2","Word3","Word4",
+            "Word5","Word6","Word7","Word8","Word9","Word10"};
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        super.onCterminalreate(savedInstanceState);
+        super.onCreate(savedInstanceState);
         // Get the view from activity_main.xml
         setContentView(R.layout.activity_main);
 
@@ -32,13 +29,59 @@ public class MainActivity extends Activity {
                 Intent myIntent = new Intent(MainActivity.this,
                         NewActivity.class);
                 startActivity(myIntent);
+
+              //  ArrayAdapter adapter = new ArrayAdapter<String>(this,
+                //   R.layout.activity_listview, mobileArray);
+
+              //  ListView listView = (ListView) findViewById(R.id.mobile_list);
+               // listView.setAdapter(adapter);
+
             }
         });
-    }
 
+
+        button = (Button) findViewById(R.id.quant);
+
+        // Capture button clicks
+        button.setOnClickListener(new OnClickListener() {
+            public void onClick(View arg0) {
+
+                // Start NewActivity.class
+                Intent myIntent = new Intent(MainActivity.this,
+                        NewActivity2.class);
+                startActivity(myIntent);
+
+            }
+        });
+
+
+        button = (Button) findViewById(R.id.nextprev);
+
+        // Capture button clicks
+        button.setOnClickListener(new OnClickListener() {
+            public void onClick(View arg0) {
+
+                // Start NewActivity.class
+             //   Intent myIntent = new Intent(MainActivity.this,
+               //         NextprevActivity.class);
+                // startActivity(myIntent);
+
+                //  ArrayAdapter adapter = new ArrayAdapter<String>(this,
+                //   R.layout.activity_listview, mobileArray);
+
+                //  ListView listView = (ListView) findViewById(R.id.mobile_list);
+                // listView.setAdapter(adapter);
+
+            }
+        });
+
+
+    }
    // @Override
-    //public boolean onCreateOptionsMenu(Menu menu) {
-      //  getMenuInflater().inflate(R.menu.activity_main, menu);
-        //return true;
+    //protected void onCreate(Bundle savedInstanceState) {
+      //  super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_main);
+
     //}
+
 }
--- app/src/main/java/com/example/anil/myapplication/NewActivity.java
+++ app/src/main/java/com/example/anil/myapplication/NewActivity.java
@@ -2,13 +2,25 @@ package com.example.anil.myapplication;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+
 
 public class NewActivity extends Activity {
+    String[] mobileArray = {"Word1","Word2","Word3","Word4",
+            "Word5","Word6","Word7","Word8","Word9","Word10"};
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         // Get the view from new_activity.xml
         setContentView(R.layout.new_activity);
+          ArrayAdapter adapter = new ArrayAdapter<String>(this,
+           R.layout.activity_listview, mobileArray);
+        ListView listView = (ListView) findViewById(R.id.mobile_list);
+        listView.setAdapter(adapter);
+
+
     }
 }
